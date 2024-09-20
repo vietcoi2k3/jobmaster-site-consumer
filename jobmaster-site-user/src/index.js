915a8eb30b498a22697c8692bf6affb6dd5402ae
vietdq <viet1234561231@gmail.com>
2024-09-20 23:59:02 +0700

    a

--- jobmaster-site-user/src/index.js
+++ jobmaster-site-user/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'antd/dist/reset.css'; // Ant Design CSS reset
+import './index.css'; // Tailwind CSS
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
